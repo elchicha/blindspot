ed947835cd34eaab05e8871d0f004d72ce6e628b
Luis Lopez-Echeto <github.com.bulginess438@passmail.com>
2026-07-09 09:08:06 -0700

    refactor: break down login function into smaller functions

--- src/blindspot/auth.py
+++ src/blindspot/auth.py
@@ -5,44 +5,74 @@ from blinkpy.auth import Auth, BlinkTwoFARequiredError
 from blinkpy.blinkpy import Blink
 
 import logging
+
 _LOGGER = logging.getLogger(__name__)
 
 
 class SessionManager:
+    USERNAME_ENV_VAR = "BLINK_USERNAME"
+    PASSWORD_ENV_VAR = "BLINK_PASSWORD"
+    EXCLUDED_CREDENTIAL_FIELDS = {"password"}
+    CREDENTIALS_ENCODING = "utf-8"
 
     def __init__(self, creds_file):
         self.creds_file = creds_file
 
     async def login(self):
-        if os.path.exists(self.creds_file):
-            with open(self.creds_file, "r") as f:
-                creds_loaded = json.load(f)
-            auth = Auth(creds_loaded, no_prompt=True)
-        else:
-            username = os.environ.get("BLINK_USERNAME")
-            password = os.environ.get("BLINK_PASSWORD")
+        auth = self._create_auth()
+        await self._startup_auth(auth)
+        self._save_credentials(auth)
+        return await self._create_blink_client(auth)
 
-            if not username or not password:
-                raise OSError("BLINK_USERNAME and BLINK_PASSWORD must be set")
-            auth = Auth(
-                {
-                    "username": os.environ["BLINK_USERNAME"],
-                    "password": os.environ["BLINK_PASSWORD"],
-                },
-                no_prompt=True,
-            )
+    def _create_auth(self):
+        if os.path.exists(self.creds_file):
+            return Auth(self._load_cached_credentials(), no_prompt=True)
+
+        return Auth(self._load_environment_credentials(), no_prompt=True)
+
+    def _load_cached_credentials(self):
+        _LOGGER.info("Loading cached credentials at %s", self.creds_file)
+        with open(self.creds_file, "r") as credentials_file:
+            return json.load(credentials_file)
+
+    def _load_environment_credentials(self):
+        _LOGGER.info("Using environment stored credentials.")
+        username = os.environ.get(self.USERNAME_ENV_VAR)
+        password = os.environ.get(self.PASSWORD_ENV_VAR)
+
+        if not username or not password:
+            _LOGGER.error("Environment credentials missing.")
+            raise OSError("BLINK_USERNAME and BLINK_PASSWORD must be set")
+
+        return {
+            "username": username,
+            "password": password,
+        }
+
+    @staticmethod
+    async def _startup_auth(auth):
         try:
+            _LOGGER.info("Starting Authorization request through blinkpy")
             await auth.startup()
         except BlinkTwoFARequiredError:
+            _LOGGER.debug("Blink two-factor-authentication code requested")
             login_pin = input("Enter your 2FA PIN: ")
             await auth.complete_2fa_login(login_pin)
 
-        creds_to_save = {
-            k: v for k, v in auth.login_attributes.items() if k not in ["password"]
+    def _save_credentials(self, auth):
+        credentials_to_save = {
+            key: value
+            for key, value in auth.login_attributes.items()
+            if key not in self.EXCLUDED_CREDENTIAL_FIELDS
         }
-        with open(self.creds_file, "w") as f:
-            json.dump(creds_to_save, f, indent=2)
 
+        with open(
+            self.creds_file, "w", encoding=self.CREDENTIALS_ENCODING
+        ) as credentials_file:
+            json.dump(credentials_to_save, credentials_file, indent=2)
+
+    @staticmethod
+    async def _create_blink_client(auth):
         blink = Blink(session=auth.session)
         blink.auth = auth
         blink.setup_urls()

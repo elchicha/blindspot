32513f140f94a462772dc36ee68fb1dc56f248c8
Luis Lopez-Echeto <github.com.bulginess438@passmail.com>
2026-07-09 14:10:32 -0700

    fix: fix get_sync_module_status and corresponding test

--- src/blindspot/sync.py
+++ src/blindspot/sync.py
@@ -2,17 +2,21 @@ from blinkpy import api
 
 
 class SyncModuleManager:
+    SYNC_MODULES_KEY = "sync_modules"
 
     def __init__(self, blink):
         self.blink = blink
 
     async def get_sync_module_status(self):
         response = await api.request_homescreen(self.blink)
-        data = await response.json()
+        homescreen_data = await response.json()
+        sync_module = homescreen_data[self.SYNC_MODULES_KEY][0]
 
-        status = []
-        for sync_module in data["sync_modules"]:
-            status.append({
-                "name": sync_module.get("name"),
-            })
-        return status
+        return {
+            "name": sync_module.get("name"),
+            "status": sync_module.get("status"),
+            "local_storage_enabled": sync_module.get("local_storage_enabled"),
+            "local_storage_status": sync_module.get("local_storage_status"),
+            "last_backup": sync_module.get("last_hb"),
+            "wifi_strength": sync_module.get("wifi_strength"),
+        }
